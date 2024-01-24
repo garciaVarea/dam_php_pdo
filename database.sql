b861dfe231cadd803f1b22bdf9ae3c58c610bb8d
usuario1 <ejemplo@gmail.com>
2024-01-24 11:35:27 +0100

    primer envio

---
+++ database.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `practica`.`productos` (
+                                        `id` INT NOT NULL AUTO_INCREMENT,
+                                        `nombre` VARCHAR(45) NULL,
+                                        `unidades` INT NULL,
+                                        `precio` FLOAT NULL,
+                                        `caducidad` DATE NULL,
+                                        `descuento` TINYINT NULL,
+                                        `foto` VARCHAR(250) NULL,
+                                        PRIMARY KEY (`id`));
